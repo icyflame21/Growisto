0a7aa94f88baadb862facdc45c24f79fe0df3f37
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2022-07-06 03:01:04 +0530

    Script Changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var counter = 1;
 setInterval(function () {
   document.getElementById("radio" + counter).checked = true;
   counter++;
-  if (counter > 5) {
+  if (counter > 3) {
     counter = 1;
   }
 }, 3000);
